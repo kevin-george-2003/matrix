d0accabca9216a29d92c1bec103b3befe5a981aa
Kevin Kakkanattu <kevin.kakkanattu@studentkuleuven.com>
2023-06-06 18:01:29 +0200

    unmutable and mutable version done

--- matrix/src/matrix/Matrix.java
+++ matrix/src/matrix/Matrix.java
@@ -2,102 +2,147 @@ package matrix;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
+
 /**
- * 
- * Each instance of this class represents a matric with a given 
- * number of rows and
- * columns and double-valued elements
+ * Each instance of this class represents a matrix with a given number of rows and columns
+ * and double-valued elements.
  * 
  * @invar | 0 <= getRowCount()
  * @invar | 0 <= getColumnCount()
  * @invar | getRows() != null
  * @invar | getRows().length == getRowCount()
  * @invar | Arrays.stream(getRows()).allMatch(row -> row != null && row.length == getColumnCount())
- * 
- * @inmutable
  */
-
 public class Matrix {
 	
-	public int getRowCount() { throw new RuntimeException("Not yet implemented"); }
+	/**
+	 * @invar | 0 <= rowCount
+	 * @invar | 0 <= columnCount
+	 * @invar | rows != null
+	 * @invar | rows.length == rowCount
+	 * @invar | Arrays.stream(rows).allMatch(row -> row != null && row.length == columnCount)
+	 */
+	private int rowCount;
+	private int columnCount;
+	/**
+	 * @representationObject
+	 * @representationObjects
+	 */
+	private double[][] rows;
 	
-	public int getColumnCount() { throw new RuntimeException( "Not yet implemented"); }
+	public int getRowCount() { return rowCount; }
+	
+	public int getColumnCount() { return columnCount; }
 	
 	/**
-	 * 
 	 * @creates | result, ...result
 	 */
-	public double[][] getRows() { throw new RuntimeException( "Not yet implemented"); }
+	public double[][] getRows() {
+		double[][] result = new double[rowCount][columnCount];
+		for (int i = 0; i < rowCount; i++)
+			for (int j = 0; j < columnCount; j++)
+				result[i][j] = rows[i][j];
+		return result;
+	}
 	
 	/**
 	 * @pre | 0 <= rowIndex && rowIndex < getRowCount()
 	 * @pre | 0 <= columnIndex && columnIndex < getColumnCount()
 	 * @post | result == getRows()[rowIndex][columnIndex]
 	 */
-	public double getElementAt(int rowIndex, int columnIndex) { throw new RuntimeException( "Not yet implemented"); }
-
+	public double getElementAt(int rowIndex, int columnIndex) {
+		return rows[rowIndex][columnIndex];
+	}
 	
 	/**
 	 * @post | result != null
 	 * @post | result.length == getRowCount() * getColumnCount()
 	 * @post | IntStream.range(0, getRowCount()).allMatch(i ->
-	 * 		 |		IntStream.range(0, getColumnCount()).allMatch(j -> 
-	 * 		 |			result[i * getColumnCount() + j] == getRows()[i][j]
-	 * 		 |		)
-	 * 		 |	)
+	 *       |     IntStream.range(0, getColumnCount()).allMatch(j ->
+	 *       |         result[i * getColumnCount() + j] == getRows()[i][j]
+	 *       |     )
+	 *       | )
 	 * @creates | result
 	 */
-	public double[] getElementsRowMajor() { throw new RuntimeException( "Not yet implemented"); }
+	public double[] getElementsRowMajor() {
+		double[] result = new double[rowCount * columnCount];
+		for (int i = 0; i < rowCount; i++)
+			for (int j = 0; j < columnCount; j++)
+				result[i * columnCount + j] = rows[i][j];
+		return result;
+	}
 	
 	/**
 	 * @post | result != null
 	 * @post | result.length == getRowCount() * getColumnCount()
 	 * @post | IntStream.range(0, getRowCount()).allMatch(i ->
-	 * 		 |		IntStream.range(0, getRowCount()).allMatch(j -> 
-	 * 		 |			result[i * getRowCount() + j] == getRows()[i][j]
-	 * 		 |		)
-	 * 		 |	)
+	 *       |     IntStream.range(0, getColumnCount()).allMatch(j ->
+	 *       |         result[j * getRowCount() + i] == getRows()[i][j]
+	 *       |     )
+	 *       | )
 	 * @creates | result
 	 */
-	public double[] getElementsColumnMajor() { throw new RuntimeException( "Not yet implemented"); }
+	public double[] getElementsColumnMajor() {
+		double[] result = new double[rowCount * columnCount];
+		for (int i = 0; i < rowCount; i++)
+			for (int j = 0; j < columnCount; j++)
+				result[j * rowCount + i] = rows[i][j];
+		return result;
+	}
 	
 	/**
-	 * @post | result != null
+	 * @mutates | this
+	 * @post | getRowCount() == old(getRowCount())
+	 * @post | getColumnCount() == old(getColumnCount())
 	 * @post | IntStream.range(0, getRowCount()).allMatch(i ->
-	 * 		 |		IntStream.range(0, getRowCount()).allMatch(j -> 
-	 * 		 |			result.getElementAt(i, j) == getElementAt(i, j) * factor
-	 * 		 |		)
-	 * 		 | )
-	 * @creates | result
+	 *       |     IntStream.range(0, getColumnCount()).allMatch(j ->
+	 *       |         getElementAt(i, j) == old(getRows())[i][j] * factor
+	 *       |     )
+	 *       | )
 	 */
+	public void scale(double factor) {
+		for (int i = 0; i < rowCount; i++)
+			for (int j = 0; j < columnCount; j++)
+				rows[i][j] *= factor;
+	}
 
-	public Matrix scaled(double factor) { throw new RuntimeException( "Not yet implemented"); }
-	
 	/**
 	 * @pre | other != null
 	 * @pre | other.getRowCount() == getRowCount()
 	 * @pre | other.getColumnCount() == getColumnCount()
-	 * @inspects | this, other
-	 * @post | result != null
+	 * @mutates | this
+	 * @inspects | other
+	 * @post | getRowCount() == old(getRowCount())
+	 * @post | getColumnCount() == old(getColumnCount())
 	 * @post | IntStream.range(0, getRowCount()).allMatch(i ->
-	 * 		 |		IntStream.range(0, getRowCount()).allMatch(j -> 
-	 * 		 |			result.getElementAt(i, j) == getElementAt(i, j) + other.getElementAt(i, j)
-	 * 		 |		)
-	 * 		 | )
-	 * @creates | result
+	 *       |     IntStream.range(0, getColumnCount()).allMatch(j ->
+	 *       |         getElementAt(i, j) == old(getRows())[i][j] + other.getElementAt(i, j)
+	 *       |     )
+	 *       | )
 	 */
-	public Matrix plus(Matrix other) { throw new RuntimeException( "Not yet implemented"); }
+	public void add(Matrix other) {
+		double[] otherElementsRowMajor = other.getElementsRowMajor();
+		for (int i = 0; i < rowCount; i++)
+			for (int j = 0; j < columnCount; j++)
+				rows[i][j] += otherElementsRowMajor[i * columnCount + j];
+	}
 	
 	/**
-	 * 
 	 * @throws IllegalArgumentException | rowCount < 0
 	 * @throws IllegalArgumentException | columnCount < 0
 	 * @throws IllegalArgumentException | elementsRowMajor == null
 	 * @throws IllegalArgumentException | elementsRowMajor.length != rowCount * columnCount
-	 * @inspect | elementsRowMajor
+	 * @inspects | elementsRowMajor
 	 * @post | getRowCount() == rowCount
 	 * @post | getColumnCount() == columnCount
 	 * @post | Arrays.equals(getElementsRowMajor(), elementsRowMajor)
 	 */
-	public Matrix(int rowCount, int columnCount, double[] elementsRowMajor) { throw new RuntimeException( "Not yet implemented"); }
+	public Matrix(int rowCount, int columnCount, double[] elementsRowMajor) {
+		this.rowCount = rowCount;
+		this.columnCount = columnCount;
+		rows = new double[rowCount][columnCount];
+		for (int i = 0; i < rowCount; i++)
+			for (int j = 0; j < columnCount; j++)
+				rows[i][j] = elementsRowMajor[i * columnCount + j];
+	}
 }
